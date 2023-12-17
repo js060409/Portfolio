10a514b3900016f65742abc8116f821f734d5a67
Howard <101975257+js060409@users.noreply.github.com>
2023-12-17 22:58:46 +0900

    Update and rename traffic.py to traffic_case_1.py

--- Team_Project/김건우 프로그램 도와주기/traffic_case_1.py
+++ Team_Project/김건우 프로그램 도와주기/traffic_case_1.py
@@ -4,12 +4,7 @@ situation = ['비', '폭설', '공사 중' ,'맑은 날', '다중 추돌사고',
 number = int(random.randint(0,5))
 weather = situation[number]
 
-#case 1 
 user_answer_1 = int((input('현재  %s 상태입니다. 북도로 우회하시겠습니까?우회를 원하시면 1을 아니면 2를 선택하십시오. :  ' %weather)))
-
-# case 2
-# user_answer_1 = int(input('현재 {} 상태입니다. 북도로 우회하시겠습니까?우회를 원하시면 1을 아니면 2를 선택하십시오. :  '.format(weather))
-
 
 if user_answer_1 == 1:
     print('알겠습니다.')
@@ -22,8 +17,3 @@ elif user_answer_1 == 2:
 
     elif user_answer_2 == 2:
         print('옳은 선택입니다.')
-
-# 처음 코드가 오류가 났던 이유
-# input()은 하나의 값만 받을수 있는데 여러개를 받으려고 함
-# 그래서 format 함수나 %s 사용해서 개선함
-# 2023_12_17 박종상, 김건우
