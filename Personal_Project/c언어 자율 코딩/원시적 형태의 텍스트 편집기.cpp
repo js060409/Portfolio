964640db19638c13ce1c0d9a639247c97addd59c
Howard <101975257+js060409@users.noreply.github.com>
2024-05-29 22:44:07 +0900

    Update 원시적 형태의 텍스트 편집기.cpp

--- Personal_Project/c언어 자율 코딩/원시적 형태의 텍스트 편집기.cpp
+++ Personal_Project/c언어 자율 코딩/원시적 형태의 텍스트 편집기.cpp
@@ -7,7 +7,7 @@ int main() {
     char filename[100];
     int flag;
 
-    printf("생성할 메모장 파일의 이름을 입력하세요 (확장자를 포함하여): ");
+    printf("생성할 메모장 파일의 이름을 입력 (확장자명 포함): ");
     fgets(filename, sizeof(filename), stdin);
 
     filename[strcspn(filename, "\n")] = '\0';
@@ -15,17 +15,19 @@ int main() {
     file_pointer = fopen(filename, "w");
 
     if (file_pointer == NULL) {
-        printf("파일을 열 수 없습니다.\n");
+        printf("error\n");
         return 1;
     }
 
-    printf("메모장에 작성할 내용을 입력하세요. 작성을 마치려면 엔터를 두 번 누르세요:\n");
+    printf("편집기에 작성할 내용을 입력. 끝내려면 엔터를 두 번:\n");
 
     do {
         fgets(input, sizeof(input), stdin);
-        // 사용자가 엔터만 입력하면 종료
-        flag = (input[0] == '\n') ? 0 : 1;
-        // 엔터가 아닌 경우에만 파일에 입력 내용을 저장
+        if (input[0] == '\n') {
+            flag = 0;
+        } else {
+            flag = 1;
+        }
         if (flag) {
             fprintf(file_pointer, "%s", input);
         }
@@ -33,7 +35,7 @@ int main() {
 
     fclose(file_pointer);
 
-    printf("%s 파일이 성공적으로 생성되었습니다.\n", filename);
+    printf("%s 파일이 생성 완료.\n", filename);
 
     return 0;
 }
